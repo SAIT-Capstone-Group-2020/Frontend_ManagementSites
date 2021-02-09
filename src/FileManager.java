05d5fae1c3ce38ac426dd398991e801b8c41596e
AlecsMG <alecs.glaban@edu.sait.ca>
2021-02-09 13:15:30 -0700

    Improved read logic and setup for Item creation

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,17 +16,15 @@ public class FileManager {
     }
 
     // ArrayList<Item>
-    public void readFile() throws FileNotFoundException {
+    public void readFile() throws IOException {
 
-        Scanner fileData = new Scanner(new File(this.fileName));
+        BufferedReader fileData = new BufferedReader(new FileReader(this.fileName));
+        System.out.println(fileData.readLine());
 
-        System.out.println("File name: " + this.fileName);
+        // change this for loop to a while loop, ending at end of file
+        for (int i = 0; i < 10; i++) {
 
-        System.out.println(fileData.nextLine());
-
-        while (fileData.hasNext()) {
-
-            String fileLine = fileData.nextLine();
+            String fileLine = fileData.readLine();
 
             String[] itemParts = fileLine.split(",");
 
@@ -34,13 +35,20 @@ public class FileManager {
             int quantity = Integer.parseInt(itemParts[4]);
             double weight = Double.parseDouble(itemParts[5]);
             String weightType = itemParts[6];
+            // add "Brand" to CSV file
             String brand = itemParts[7];
 
-            System.out.printf("%s %10s %10f %10s %10d %10f %10s %10s %n", productId, productName, price, category,
-                    quantity, weight, weightType, brand);
+            // This line is used to print the line to the console, to test if it's read properly
+            /*
+             * System.out.printf("%s %10s %10f %10s %10d %10f %10s %10s %n", productId,
+             * productName, price, category, quantity, weight, weightType, brand);
+             */
 
-            // Item newItem = new Item(productId, productName, price, category, quantity,
-            // weight, weightType, brand);
+            // This line is used to create a new object when the backend connection is made
+            /*
+             * Item newItem = new Item(productId, productName, price, category, quantity,
+             * weight, weightType, brand);
+             */
         }
 
     }

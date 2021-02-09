ac47db4e22007711471b207ae415aeaaff5623c3
AlecsMG <alecs.glaban@edu.sait.ca>
2021-02-09 13:49:17 -0700

    Fixed file loop

--- src/FileManagerDriver.java
+++ src/FileManagerDriver.java
@@ -1,7 +1,12 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.util.Scanner;
+
+/*
+* NOTE: THIS FILE WON'T EXIST IN THE FINAL ENVIRONMENT. IT IS MERELY HERE TO TEST 
+*       THE FILEMANAGER CLASS. FILEMANAGER WILL HAVE THE METHOD TO RECEIVE 
+*       THE FILE PATH FROM THE JS ELEMENT THAT CALLS IT.
+*/
 
 public class FileManagerDriver {
     public static void main(String args[]) throws IOException {
@@ -15,6 +20,6 @@ public class FileManagerDriver {
 
         fileManager.readFile();
 
-        // fileName.close();
+        fileName.close();
     }
 }

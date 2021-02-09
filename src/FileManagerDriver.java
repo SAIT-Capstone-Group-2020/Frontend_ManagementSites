fe7aaf98fd967a44632038b673b47834838c67e8
AlecsMG <alecs.glaban@edu.sait.ca>
2021-02-09 13:16:17 -0700

    changed from Scanner to Buffered Reader, to allow for CSV file reading

--- src/FileManagerDriver.java
+++ src/FileManagerDriver.java
@@ -1,9 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileManagerDriver {
-    public static void main(String args[]) throws FileNotFoundException {
+    public static void main(String args[]) throws IOException {
         System.out.print("Enter the file path: ");
 
         Scanner fileName = new Scanner(System.in);
